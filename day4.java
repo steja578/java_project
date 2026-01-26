fc4170eb12169e1680d0e453598206d224e7b2a1
Bharath_tn <steja11100@gmail.com>
2026-01-25 22:34:13 -0600

    ? ternary operator

--- day4.java
+++ day4.java
@@ -42,27 +42,49 @@
 
 
 //************Conditional statements***************** */
-class day4{
+// class day4{
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
         
-        int x = 8;
-        int y = 7;
-        int z = 9;
+//         int x = 8;
+//         int y = 7;
+//         int z = 9;
 
-        if(x>y && x>z)
-        {
-            System.out.println(x);
+//         if(x>y && x>z)
+//         {
+//             System.out.println(x);
 
-        }
-        else if (y>z)
-         {
-         System.out.println(y);
+//         }
+//         else if (y>z)
+//          {
+//          System.out.println(y);
 
-        }
-        else 
-        {
-            System.out.println(z);
-        }
+//         }
+//         else 
+//         {
+//             System.out.println(z);
+//         }
+//     }
+// }
+
+class day4
+{
+    public static void main(String[] args) {
+        int n=5;
+        int result =0;
+
+        // if(n%2==0)
+        // {
+        //     result = 10;
+        // }
+        // else{
+        //     result = 20;
+        // }
+
+        // **************? using ternary operator*******************
+
+        result = n%2==0 ? 10 : 20;
+        System.out.println(result);
+
     }
 }
